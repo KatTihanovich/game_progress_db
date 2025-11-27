42bb87d3473274a551b4a655f23bc2df4f97e46f
KatTihanovich <128325686+KatTihanovich@users.noreply.github.com>
2025-11-27 13:10:51 +0200

    Modify timestamps to be NOT NULL in achievements table

--- db/migration/V5__create_table_achievements.sql
+++ db/migration/V5__create_table_achievements.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS achievements (
     achievement_id SERIAL PRIMARY KEY,
     achievement_name TEXT NOT NULL UNIQUE,
     achievement_description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT achievements_name_not_empty CHECK (LENGTH(TRIM(achievement_name)) > 0)
 );
 
