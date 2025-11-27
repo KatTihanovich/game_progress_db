41cf133f7b3cdf284e97cf35e82cccb88825f3bf
KatTihanovich <128325686+KatTihanovich@users.noreply.github.com>
2025-11-27 13:11:37 +0200

    Modify created_at and updated_at to NOT NULL

--- db/migration/V6__create_table_users_achievements.sql
+++ db/migration/V6__create_table_users_achievements.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS users_achievements (
     user_achievement_id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL,
     achievement_id INTEGER NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_user_achievements_user FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT fk_user_achievements_achievement FOREIGN KEY (achievement_id) REFERENCES achievements(achievement_id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT users_achievements_unique UNIQUE (user_id, achievement_id)
